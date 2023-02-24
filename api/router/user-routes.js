0e4836d649a4941316f8afa0736ee64dfb2cfb97
Joynae Whitehurst <Joynaew@gmail.com>
2023-02-24 14:41:29 -0500

    html

--- api/router/user-routes.js
+++ api/router/user-routes.js
@@ -6,7 +6,7 @@ import * as controller from '../controller/user-controller.js'
 const userrouter = Router()
 
 userrouter.get('/', controller.getAllUser)
-userrouter.put('/signup', controller.signup)
+userrouter.post('/signup', controller.signup)
 userrouter.post('/login', controller.login)
 
 
