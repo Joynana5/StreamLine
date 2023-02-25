9023ec96a06af36c672b594032ce089d23a4c0ae
Joynae Whitehurst <Joynaew@gmail.com>
2023-02-25 10:27:55 -0500

    update
    fixed login and signup?

--- public/index.js
+++ public/index.js
@@ -1,14 +1,16 @@
-const wrapper = document.querySelector('wrapper')
+const wrapper = document.querySelector('.wrapper')
 const signupHeader = document.querySelector('.signup header')
 const loginHeader = document.querySelector('.login header')
+
 
 // @ts-ignore
 loginHeader.addEventListener('click', () => {
   // @ts-ignore
   wrapper.classList.add('active')
 })
+
 // @ts-ignore
-loginHeader.addEventListener('click', () => {
+signupHeader.addEventListener('click', () => {
   // @ts-ignore
   wrapper.classList.remove('active')
 })
