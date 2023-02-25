6ec7e999f0a7b5b4df3f04aff468da3f8fc3be4b
Joynae Whitehurst <Joynaew@gmail.com>
2023-02-25 11:25:38 -0500

    css

--- public/views.js
+++ public/views.js
@@ -1,3 +1,7 @@
+import data from '../api/index.js'
+import employee from '../api/router/employee-router.js'
+import user from '../api/router/user-routes.js'
+
 const wrapper = document.querySelector('.wrapper')
 const signupHeader = document.querySelector('.signup header')
 const loginHeader = document.querySelector('.login header')
